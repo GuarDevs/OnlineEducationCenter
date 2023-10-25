919e6056d2a7f75c62240e26ee7852b84cfaf2b5
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-10-25 19:14:40 +0500

    Student changes

--- OnlineEducationCenter.Web/Models/Students/Student.cs
+++ OnlineEducationCenter.Web/Models/Students/Student.cs
@@ -1,6 +1,22 @@
-﻿namespace OnlineEducationCenter.Web.Models.Students
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineEducationCenter.Web.Models.Students
 {
     public class Student
     {
+        [Parameter]
+        public string FirstName { get; set; }
+
+        [Parameter]
+        public string LastName { get; set; }
+
+        [Parameter]
+        public DateTime Date { get; set; }
+
+        [Parameter]
+        public string Phone { get; set; }
+
+        [Parameter]
+        public string Email { get; set; }
     }
 }
