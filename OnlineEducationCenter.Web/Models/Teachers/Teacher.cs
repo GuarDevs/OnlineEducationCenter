1deddb24bd6c3d317f3cfdb4267f49fa26a85119
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-10-25 19:16:58 +0500

    Teacher changes

--- OnlineEducationCenter.Web/Models/Teachers/Teacher.cs
+++ OnlineEducationCenter.Web/Models/Teachers/Teacher.cs
@@ -1,6 +1,21 @@
-﻿namespace OnlineEducationCenter.Web.Models.Teachers
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineEducationCenter.Web.Models.Teachers;
+
+public class Teacher
 {
-    public class Teacher
-    {
-    }
+    [Parameter]
+    public string FirstName { get; set; }
+
+    [Parameter]
+    public string LastName { get; set; }
+
+    [Parameter]
+    public DateTime Date { get; set; }
+
+    [Parameter]
+    public string Phone { get; set; }
+
+    [Parameter]
+    public string Email { get; set; }
 }
